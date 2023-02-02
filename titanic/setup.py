a096c1d201a9b155aaa5c5b7dcb7f97db80c94fc
Evgeny Melnikov <emelnikov@microsoft.com>
2023-02-02 06:08:05 -0800

    Project structure init

---
+++ titanic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data science python training',
+    author='Evgeny Melnikov',
+    license='MIT',
+)
